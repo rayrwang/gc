ecdf36a500cc66824dfe393c985b13618391f7bc
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-04-14 00:26:38 +0100

    Add project root to path when running from `examples/`

--- examples/00_mnist_representations.py
+++ examples/00_mnist_representations.py
@@ -1,4 +1,8 @@
 
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
 import multiprocessing
 import argparse
 import itertools
--- examples/01_mnist_active.py
+++ examples/01_mnist_active.py
@@ -1,4 +1,8 @@
 
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
 import datetime
 import multiprocessing
 import argparse
